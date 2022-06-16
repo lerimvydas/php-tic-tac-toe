3f03e348887e629c542fc545979a472bb175c7b4
Rimvydas <rimvydaslevinskis@gmail.com>
2022-06-17 02:00:22 +0300

    code cleanup

--- src/Command/SocketServerCommand.php
+++ src/Command/SocketServerCommand.php
@@ -8,10 +8,8 @@ use Ratchet\Http\HttpServer;
 use Ratchet\Server\IoServer;
 use Ratchet\WebSocket\WsServer;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Console\Attribute\AsCommand;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -23,24 +21,13 @@ class SocketServerCommand extends Command
 {
     protected function configure(): void
     {
-        $this
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
-        }
-
-        if ($input->getOption('option1')) {
-            // ...
-        }
-
+        $io->note('Starting TicTacToe game socket server');
 
         $server = IoServer::factory(
             new HttpServer(
@@ -50,10 +37,8 @@ class SocketServerCommand extends Command
             ),
             10000
         );
-    
-        $server->run();
 
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        $server->run();
 
         return Command::SUCCESS;
     }
